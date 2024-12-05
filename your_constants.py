d68c1592d34c8affeaa64adcaca26cd90aad4bb1
SPolimeni <simone.polimeni@rse-web.it>
2024-12-05 17:09:55 +0100

    venv

--- your_constants.py
+++ your_constants.py
@@ -14,4 +14,4 @@ TIMEOUT_REWARD = -1.0
 NUM_DIRECTIONS = 8
 TIMEOUT = 1000
 
-YOUR_ROOT = '/Users/jmugan/ray_results/YourTrainer'
+YOUR_ROOT = 'C:\\Users\\polimeni\\OneDrive - RSE S.p.A\\Documenti\\PTR_25_27\\GitHubRepos\\practical_rllib_tutorial
